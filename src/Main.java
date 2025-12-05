4eb152bef1e12ff2bf7324aba6443d1e31249bf1
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2025-12-04 22:22:48 -0600

    Eliminación de archivo basura Main.java

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    static void main() {
-        IO.print("Ingresa tu edad: ");
-        int edad = Integer.parseInt(IO.readln());
-        IO.println("Tu edad es: " + edad);
-    }
-}
